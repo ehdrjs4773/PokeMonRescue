6ed9d422407686cd0d3f1fb17ae8594bb9d26368
초코달링 <rudekrudek@naver.com>
2018-02-13 01:41:44 +0900

    준우씨가 만들어둔 포켓몬 헤더와 건하가 올린 헤더가 겹쳐서 그거 수정했습니다. 원래 준우씨가 만들어놨던 헤더의 내용을 enemy.h에 모두 추가하였으니 준우씨 확인 부탁드립니다. 터트리는 것보다는.... 이게 나을 거 같아서 컴파일 확인된거 보고서 올립니다.

--- HeptaAPI/enemy.h
+++ HeptaAPI/enemy.h
@@ -1,8 +1,56 @@
 #pragma once
 #include "stdafx.h"
 #include "gameNode.h";
-#include "Pokemon.h"
 //임시포켓몬
+
+struct tagTempPokemon
+{
+	image* idleImage;
+	image* moveImage;
+	image* atkImage;
+	image* hurtImage;
+
+	char* idleImageName;
+	char* moveImageName;
+	char* atkImageName;
+	char* hurtImageName;
+
+	float x;
+	float y;
+	int direction;
+	int state;
+};
+
+
+//현재 상태 바라보는 방향
+enum direction
+{
+	UP,
+	LEFTUP,
+	LEFT,
+	LEFTDOWN,
+	DOWN,
+	RIGHTDOWN,
+	RIGHT,
+	RIGHTUP,
+};
+
+enum state
+{
+	STATE_IDLE,
+	STATE_ATTACK,
+	STATE_MOVE,
+	STATE_HURT
+};
+
+//이미지의 이름을 구조체화로 하여 부모클래스에 넘겨서 따로따로 할 필요 없이 한개의 함수로 끝내기 위한 편의성 구조체
+struct tagImageName
+{
+	char* idleImage;
+	char* moveImage;
+	char* attackImage;
+	char* hurtImage;
+};
 
 
 class enemy : public gameNode

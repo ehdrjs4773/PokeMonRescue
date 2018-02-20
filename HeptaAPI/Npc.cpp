13367c18d0da00f37329353771918d1f33849457
DESKTOP-GKHV021\최동건 <ehdrjs4773@naver.com>
2018-02-20 16:30:33 +0900

    // NPC    수정!

--- HeptaAPI/Npc.cpp
+++ HeptaAPI/Npc.cpp
@@ -34,10 +34,16 @@ HRESULT Npc::init()
 
 	_town.inout = true;
 	
-	_player->init("¸®ÀÚ¸ù", WINSIZEX / 2, WINSIZEY / 2 , true);
+	_player->init("¸®ÀÚ¸ù");
+	_player->setPosition(WINSIZEX / 2 , WINSIZEY - 50);
+
 	//´Ù ‰ç¾î¿ä Âô±ß ><*
 
+	_rc = RectMakeCenter(WINSIZEX / 2 - 60, WINSIZEY / 2 + 10, 30, 30);
+	_rc2 = RectMakeCenter(WINSIZEX / 2, WINSIZEY / 2+ 130, 50, 30);
+	_house = false;
 
+	CAMERAMANAGER->init(480, 360, WINSIZEX, WINSIZEY);
 	return S_OK;
 }
 
@@ -51,17 +57,38 @@ void Npc::release()
 void Npc::update()
 {
 	_player->update();
-	
+	_player->townMove();
 	RECT temp;
-	if (IntersectRect(&temp, &_npc1.collsionrc, &_player->getRect()))
-	{	
-		if (KEYMANAGER->isOnceKeyDown(PLAYER_SELECT_KEY))
+	if (_town.inout)
+	{
+		if (IntersectRect(&temp, &_npc1.collsionrc, &_player->getRect()))
 		{
-			SCENEMANAGER->changeChild("shopchiled");
-			SCENEMANAGER->init("npc");
+			if (KEYMANAGER->isOnceKeyDown(PLAYER_SELECT_KEY))
+			{
+				SCENEMANAGER->changeChild("shopchiled");
+				SCENEMANAGER->init("npc");
+			}
+		}
+
+		if (IntersectRect(&temp, &_rc, &_player->getRect()))
+		{
+			_town.inout = false;
+			_player->setPosition(_rc2.left + 25, WINSIZEY / 2 + 100);
+
 		}
 	}
 
+
+	if (!_town.inout)
+	{
+		if (IntersectRect(&temp, &_rc2, &_player->getRect()))
+		{
+			_town.inout = true;
+			_player->setPosition(_rc.left + 15, WINSIZEY / 2 + 45);
+		}
+	}
+
+	
 }
 
 
@@ -72,16 +99,20 @@ void Npc::render()
 	
 	if(_town.inout)
 	{
+		IMAGEMANAGER->findImage("townMagenta")->render(dc, 0, 0);
 		IMAGEMANAGER->findImage("town")->render(dc, 0, 0);
 		_npc1.image->render(dc, _npc1.x - _npc1.image->getWidth() / 2, _npc1.y - _npc1.image->getHeight() / 2);
 		_npc2.image->render(dc, _npc2.x - _npc2.image->getWidth() / 2, _npc2.y - _npc2.image->getHeight() / 2);
 		Rectangle(dc, _npc1.collsionrc.left, _npc1.collsionrc.top, _npc1.collsionrc.right, _npc1.collsionrc.bottom);
 		Rectangle(dc, _player->getRect().left, _player->getRect().top, _player->getRect().right, _player->getRect().bottom);
+		Rectangle(dc, _rc.left, _rc.top, _rc.right, _rc.bottom);
 	}
 
 	if (!_town.inout)
 	{
+		IMAGEMANAGER->findImage("alphamap")->alphaRender(CAMERAMANAGER->getMemDC(),	0, 0, 255);
 		IMAGEMANAGER->findImage("saveTown")->render(dc, WINSIZEX / 2 - IMAGEMANAGER->findImage("saveTown")->getWidth() / 2, WINSIZEY / 2 - IMAGEMANAGER->findImage("saveTown")->getHeight() / 2);
+		Rectangle(dc, _rc2.left, _rc2.top, _rc2.right, _rc2.bottom);
 	}
 
 	_player->render();
--- HeptaAPI/Npc.h
+++ HeptaAPI/Npc.h
@@ -25,10 +25,13 @@ class Npc : public gameNode
 {
 private:
 	
-	npc _npc1;	// 일단 시험 삼아 만든 상인 1
-	npc _npc2;	// 일단 시험 삼아 만든 상인 2
+	npc _npc1;		// 일단 시험 삼아 만든 상인 1
+	npc _npc2;		// 일단 시험 삼아 만든 상인 2
 	
-	town _town;	// 타운이죠~
+	town _town;		// 타운이죠~
+	bool _house;	// 하우스 들어갔나요오~
+	RECT _rc;		// 입구 죠~
+	RECT _rc2;		// 출구 죠~
 
 	player* _player;
 

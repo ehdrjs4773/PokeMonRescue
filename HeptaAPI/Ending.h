681f5eec733ea9e7de5be46f631df8c2812396b6
JAEYOON <log901108@gmail.com>
2018-02-22 15:24:51 +0900

    엔딩 및 오프닝 업데이트, 이미지 업로드

---
+++ HeptaAPI/Ending.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "gameNode.h"
+
+
+class Ending : public gameNode
+{
+private:
+	int _x, _y;
+
+
+public:
+	Ending();
+	~Ending();
+
+
+	virtual HRESULT init();
+	virtual void release(void);
+	virtual void update(void);
+	virtual void render(void);
+
+};
+
